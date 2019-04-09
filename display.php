a55e80c7da9a3d73d01c6a17a67abe814a410191
Nightvg <night@laptop.de>
2019-04-09 14:02:58 +0200

    Update Display

--- display.php
+++ display.php
@@ -7,15 +7,19 @@ $conn = OpenCon();
 $f = Select('teams','teamid');
 $anz = sizeof($f);
 $teams = array(Select('teams','points'), Select('teams','name'), Select('teams','teamleiter'), Select('teams','roboter'), Select('teams','teamid'));
+$games = array(Select('games','gameid'),Select('games','block'),Select('games','time'),Select('games','points'),Select('games','team'),Select('games','penalty'),Select('games','objectives'),Select('games','active'),Select('games','finished'));
 
 array_multisort($teams[0], SORT_DESC, $teams[1], $teams[2], $teams[3], $teams[4]);
 //0 -> Punkte; 1 -> Name; 2 -> Teamleiter; 3 -> Roboter; 4 -> TeamID
+
+array_multisort($games[0], $games[1], SORT_ASC, $games[2], $games[3], $games[4], $games[5], $games[6], $games[7], $games[8]);
+//0 -> GameID; 1 -> Block; 2 -> Zeit; 3 -> Punkte; 4 -> TeamID; 5 -> Strafenanzahl; 6 -> Objectives; 7 -> Status; 8 -> Beendet
 ?>
 <head>
 <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
-<table style="width:100%" class='display'>
+<table style='width:100%' class='display'>
 	<tr>
 		<td style='width:300px'>Teams</td><td>Spielblock I</td><td>Spielblock II</td><td>Spielblock III</td><td>Spielblock IV</td><td>Spielblock V</td>
 	</tr>
@@ -31,21 +35,29 @@ array_multisort($teams[0], SORT_DESC, $teams[1], $teams[2], $teams[3], $teams[4]
 			echo"</td><td rowspan='2' style='width:25px'>";
 			echo $teams[0][$i]; //Punktzahl
 			echo "</td></tr><tr><td style='width:125px'>";
-			echo $te ams[2][$i]; //Teamleiter
+			echo $teams[2][$i]; //Teamleiter
 			echo "</td><td style='width:125px'>";
 			echo $teams[3][$i]; //Roboter
 			echo "</td></tr></table><br>";
 			echo "</td>";
 			
 			$s = 0;
-			$games = SelectWhere('games','team='.$teams[4][$i]);
-			//0 -> GameID; 1 -> Block; 2 -> Zeit; 3 -> Punkte; 4 -> TeamID; 5 -> Strafenanzahl; 6 -> Objectives
-			array_multisort($games[0], $games[1], SORT_ASC, $games[2], $games[3], $games[4], $games[5], $games[6]);
 			
-			while($s < 6 && $games[2][$i] <> '00:00:00')
+			
+			//0 -> GameID; 1 -> Block; 2 -> Zeit; 3 -> Punkte; 4 -> TeamID; 5 -> Strafenanzahl; 6 -> Objectives; 7 -> Status; 8 -> Beendet
+			
+			
+			while($s < 6 && $games[7][$i])
 			{
-				
-				if($games[][])
+				if(!$games[7][$i])
+				{
+					//Gamestime checken und ausgeben als nächstes Spiel
+					break;
+				}
+				else
+				{
+					//Spiele anzeigen
+				}
 			}
 			
 		}
---
+++ testing.php
@@ -0,0 +1,8 @@
+<?php
+
+$settings = parse_ini_file("settings.ini",true);
+
+var_dump($settings);
+
+
+?>
