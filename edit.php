b50c3bd9a28618f3642a8b3cf4a13d837f0abf1e
Nightvg <night@laptop.de>
2019-05-06 21:12:15 +0200

    Editing single games is now possible

--- edit.php
+++ edit.php
@@ -5,6 +5,311 @@ include 'db_manip.php';
 $settings = parse_ini_file('settings.ini',true);
 
 $teams = Select('teams','*');
-var_dump($teams);
+
 ?>
 <p>Ein Team ausw√§hlen:</p>
+<form action='edit.php' method='post'>
+<select id='team' name='team'>
+	<option value='0'>---Auswahl---</option>
+	<?php
+	for($i = 0; isset($teams[$i]); $i++)
+	{
+		echo "<option value='".$teams[$i]['teamid']."'>".$teams[$i]['name']."</option>";
+	}
+	?>
+</select><br>
+<input type='submit' value='Akzeptieren'/>
+</form>
+<br>
+<table style='width:100%'>
+<?php
+if(isset($_POST['team']))
+{
+	$conn = OpenCon();
+	
+	$sql = "SELECT * FROM games WHERE team='".$_POST['team']."' ORDER BY block ASC";
+	$result = $conn->query($sql);
+	
+	$games = array();
+	
+	if($result->num_rows > 0)
+	{
+		while($row = $result->fetch_assoc())
+		{
+			$games[] = $row;
+		}
+	}
+	else
+	{
+		write_log("0 Results for the query: ".$sql." in edit.php");
+	}
+	
+	for($i = 0; isset($games[$i]) && $i < 7; $i++)
+	{
+		$sql = "SELECT * FROM pointmanagement WHERE game='".$games[$i]['gameid']."'";
+		$result = $conn->query($sql);
+		
+		$points = array();
+		
+		if($result->num_rows > 0)
+		{
+			while($row = $result->fetch_assoc())
+			{
+				$points = $row;
+			}
+		}
+		else
+		{
+			write_log("No Points found for game: ".$games[$i]['gameid']." in edit.php");
+		}
+		
+		echo "Block : ".$games[$i]['block'];
+		echo "<table><tr><td>";
+		echo $games[$i]['time'];
+		echo "</td><td>Objectives: ";
+		echo $games[$i]['objectives'];
+		echo "</td><td>Strafen: ";
+		echo $games[$i]['penalties'];
+		echo "</td></tr><tr><td>";
+		echo $games[$i]['points'];
+		echo "</td><td>+Punkte: ";
+		echo $points['+1'] * 1 + $points['+3'] * 3 + $points['+5'] * 5;
+		echo "</td><td>-Punkte: ";
+		echo $points['-1'] * -1 + $points['-3'] * -3 + $points['-5'] * -5;
+		echo "</td></tr><tr><td colspan='3'>";
+		echo "<form action='edit.php' method='post'><button type='submit' name='game' value='".$games[$i]['gameid']."'>Bearbeiten</button></form>";
+		echo "</td></tr></table>";
+	}
+	CloseCon($conn);
+}
+if(isset($_POST['game']))
+{
+	$conn = OpenCon();
+	
+	$sql = "SELECT * FROM games WHERE gameid='".$_POST['game']."'";
+	$result = $conn->query($sql);
+	
+	$game = array();
+	
+	if($result->num_rows > 0)
+	{
+		while($row = $result->fetch_assoc())
+		{
+			$game = $row;
+		}
+	}
+	else
+	{
+		write_log("0 Results for the query: ".$sql." in edit.php");
+	}
+	
+	$sql = "SELECT * FROM games WHERE block='".$game['block']."' AND time > '".$game['time']."' ORDER BY time ASC";
+	$result = $conn->query($sql);
+	
+	$refgames = array();
+	$size = 0;
+	
+	if($result->num_rows > 0)
+	{
+		while($row = $result->fetch_assoc())
+		{
+			$refgames[] = $row;
+			$size++;
+		}
+	}
+	else
+	{
+		write_log("Keine Spiele mehr zum tauschen?");
+	}
+	
+	
+	$sql = "SELECT * FROM pointmanagement WHERE game='".$_POST['game']."'";
+	$result = $conn->query($sql);
+	
+	$point = array();
+	
+	if($result->num_rows > 0)
+	{
+		while($row = $result->fetch_assoc())
+		{
+			$point = $row;
+		}
+	}
+	else
+	{
+		write_log("No Points found for game: ".$_POST['game']." in edit.php");
+	}
+
+	echo "<form action='edit.php' method='post'>";
+	echo "<table><tr><td>Zeit:</td><td><input type='text' name='time' value='".$game['time']."'/></td></tr>";
+	if($settings['Options']['+1_enable']) echo "<tr><td>+1:</td><td><input type='text' name='+1' value='".$point['+1']."'/></td></tr>";
+	if($settings['Options']['+3_enable']) echo "<tr><td>+3:</td><td><input type='text' name='+3' value='".$point['+3']."'/></td></tr>";
+	if($settings['Options']['+5_enable']) echo "<tr><td>+5:</td><td><input type='text' name='+5' value='".$point['+5']."'/></td></tr>";
+	if($settings['Options']['-1_enable']) echo "<tr><td>-1:</td><td><input type='text' name='-1' value='".$point['-1']."'/></td></tr>";
+	if($settings['Options']['-3_enable']) echo "<tr><td>-3:</td><td><input type='text' name='-3' value='".$point['-3']."'/></td></tr>";
+	if($settings['Options']['-5_enable']) echo "<tr><td>-5:</td><td><input type='text' name='-5' value='".$point['-5']."'/></td></tr>";
+	echo "<tr><td>Spiel tauschen</td><td><select id='timeswitch' name='timeswitch'><option value='0'>--Nur bei Spielwechsel auswaehlen--</option>";
+	for($i = 0; $i < $size; $i++)
+	{
+		$sql = "SELECT name FROM teams WHERE teamid='".$refgames[$i]['team']."'";
+		$result = $conn->query($sql);
+	
+		$name = $result->fetch_assoc();
+		
+		if($name['name'])
+		{
+			echo "<option value='".$refgames[$i]['gameid']."'>".int_to_time($refgames[$i]['time'])." ".$name['name']."</option>";
+		}
+	}
+	echo "<select></td></tr>";
+	echo "<tr><td colspan='2'><button type='submit' name='change' value='".$_POST['game']."'>Best‰tigen</button></td></tr></table></form>";
+	
+	CloseCon($conn);
+}
+if(isset($_POST['change']))
+{
+	$conn = OpenCon();
+	
+	$sql = "SELECT * FROM games WHERE gameid='".$_POST['change']."'";
+	$result = $conn->query($sql);
+	
+	$game = array();
+	
+	if($result->num_rows > 0)
+	{
+		while($row = $result->fetch_assoc())
+		{
+			$game = $row;
+		}
+	}
+	else
+	{
+		write_log("0 Results for the query: ".$sql." in edit.php");
+	}
+
+	
+	$refgame = array();
+	
+	if($_POST['timeswitch'])
+	{
+		$sql = "SELECT * FROM games WHERE gameid='".$_POST['timeswitch']."'";
+		$result = $conn->query($sql);
+	
+		if($result->num_rows > 0)
+		{
+			while($row = $result->fetch_assoc())
+			{
+				$refgame = $row;
+			}
+	
+			$sql = "UPDATE games SET time='".$refgame['time']."' WHERE gameid='".$game['gameid']."'";
+			$conn->query($sql);
+			
+			$sql = "UPDATE games SET time='".$game['time']."' WHERE gameid='".$refgame['gameid']."'";
+			$conn->query($sql);
+		}
+		else
+		{
+			write_log("0 Results for the query: ".$sql." in edit.php");
+		}
+	}
+	
+	$sql = "SELECT * FROM pointmanagement WHERE game='".$_POST['change']."'";
+	$result = $conn->query($sql);
+	
+	$point = array();
+	
+	if($result->num_rows > 0)
+	{
+		while($row = $result->fetch_assoc())
+		{
+			$point = $row;
+		}
+	}
+	else
+	{
+		write_log("No Points found for game: ".$_POST['game']." in edit.php");
+	}
+	
+	if(($_POST['time'] != $game['time']) && !$_POST['timeswitch'])
+	{
+		$sql = "UPDATE games SET time='".$_POST['time']."' WHERE gameid='".$_POST['change']."'";
+		$conn->query($sql);
+		
+		$sql = "UPDATE changed SET time='1' WHERE game='".$_POST['change']."'";
+		$conn->query($sql);
+	}
+
+	if(isset($_POST['+1']))
+	{
+		if($_POST['+1'] != $point['+1'])
+		{
+			$sql = "UPDATE pointmanagement SET `+1`='".$_POST['+1']."' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+			
+			$sql = "UPDATE changed SET objectives='1' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		}
+	}
+	if(isset($_POST['+3']))
+	{
+		if($_POST['+3'] != $point['+3'])
+		{
+			$sql = "UPDATE pointmanagement SET `+3`='".$_POST['+3']."' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		
+			$sql = "UPDATE changed SET objectives='1' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		}
+	}
+	if(isset($_POST['+5']))
+	{
+		if($_POST['+5'] != $point['+5'])
+		{
+			$sql = "UPDATE pointmanagement SET `+5`='".$_POST['+5']."' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		
+			$sql = "UPDATE changed SET objectives='1' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		}
+	}
+	if(isset($_POST['-1']))
+	{
+		if($_POST['-1'] != $point['-1'])
+		{
+			$sql = "UPDATE pointmanagement SET `-1`='".$_POST['-1']."' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		
+			$sql = "UPDATE changed SET penalties='1' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		}
+	}
+	if(isset($_POST['-3']))
+	{
+		if($_POST['-3'] != $point['-3'])
+		{
+			$sql = "UPDATE pointmanagement SET `-3`='".$_POST['-3']."' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		
+			$sql = "UPDATE changed SET penalties='1' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		}
+	}
+	if(isset($_POST['-5']))
+	{
+		if($_POST['-5'] != $point['-5'])
+		{
+			$sql = "UPDATE pointmanagement SET `-5`='".$_POST['-5']."' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		
+			$sql = "UPDATE changed SET penalties='1' WHERE game='".$_POST['change']."'";
+			$conn->query($sql);
+		}
+	}
+	UpdateGame($_POST['change']);
+	UpdateDB();
+}
+
+?>
+</table>
+<a href='/robosax/workspace.php'>Workspace</a>
