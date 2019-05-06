d86b84028449186835ab0c8ce87f24c297baf860
Nightvg <night@laptop.de>
2019-05-06 21:11:40 +0200

    Added various functions

--- db_manip.php
+++ db_manip.php
@@ -5,6 +5,47 @@ function write_log($statement)
 	fwrite($log, date("H:i:s")." : ".$statement."; \n");
 	fclose($log);
 }
+
+function int_to_time($int)
+{	
+	if($int >= 1000)
+	{
+		if($int%100 >= 10)
+		{
+			return intdiv($int,100) . ":" . $int%100;
+		}
+		else
+		{
+			return intdiv($int,100) . ":0" . $int%100;
+		}
+	}
+	else
+	{
+		if($int%100 >= 10)
+		{
+			return "0" . intdiv($int,100) . ":" . $int%100;
+		}
+		else
+		{
+			return "0" . intdiv($int,100) . ":0" . $int%100;
+		}
+	}
+}
+
+function addTimes($time1, $time2)
+{
+	$h = int($time1/100) + int($time2/100);
+	$m = int($time1%100) + int($time2%100);
+	
+	if($m > 59)
+	{
+		$h++;
+		$m -= 60; 
+	}
+	
+	return (100 * $h) + $m;
+}
+
 function Select($table, $col)
 {
 	
@@ -107,7 +148,7 @@ function UpdateDB()
 						write_log("Changed highlight of Game: ".$highlight[0][$e]." to 1 in Update()");
 					}
 				}
-				if($e >= 3 && $highlight[2][$e])
+				if(($e >= 3 && $highlight[2][$e]) || !$highlight[1][$e])
 				{
 					$sql = "UPDATE games SET highlight='0' WHERE gameid='".$highlight[0][$e]."'";
 					$conn->query($sql);
@@ -117,11 +158,11 @@ function UpdateDB()
 			
 			$sql = "UPDATE teams SET points='".$points."' WHERE teamid='".$i."'";
 			$conn->query($sql);
-			write_log("Set new points of team ".$i." to ".$points." in Updated()");
+			write_log("Set new points of team ".$i." to ".$points." in UpdatedDB()");
 		}
 		else
 		{
-			write_log("0 results for the query: ".$sql." : in using Update() (db_manip.php)");
+			write_log("0 results for the query: ".$sql." : in using UpdateDB() (db_manip.php)");
 		}
 		
 		
@@ -130,29 +171,87 @@ function UpdateDB()
 	CloseCon($conn);
 }
 
-function CheckOrder($comparr)
+function UpdateGame($id)
 {
 	$conn = OpenCon();
 	
-	$sql = "SELECT * FROM teams ORDER BY points DESC, teamid DESC";
+	$points = array();
+	$sql = "SELECT * FROM pointmanagement WHERE game ='".$id."'";
 	$result = $conn->query($sql);
-	$team = array();
+	
+	if($result->num_rows > 0)
+	{
+		$points = $result->fetch_assoc();		
+	}
+	else
+	{
+		write_log("0 results for the query: ".$sql." : in using UpdateGame() (db_manip.php)");
+	}
+	
+	$pospoints = $points['+1'] * 1 + $points['+3'] * 3 + $points['+5'] * 5;
+	$negpoints = $points['-1'] * -1 + $points['-3'] * -3 + $points['-5'] * -5;
 
+	if($pospoints + $negpoints < 0)
+	{
+		$sql = "UPDATE games SET points='0' WHERE gameid='".$id."'";
+		$conn->query($sql);
+	}
+	else
+	{
+		$sql = "UPDATE games SET points='".($pospoints + $negpoints)."' WHERE gameid='".$id."'";
+		$conn->query($sql);
+	}
+}
+
+function UpdateTime()
+{
+	$settings = parse_ini_file("settings.ini",true);
+	$conn = OpenCon();
+	
+	$sql = "SELECT teamid FROM teams WHERE active='1' ORDER BY points DESC, teamid ASC";
+	$result = $conn->query($sql);
+	
+	$teams = array();
+	
 	if($result->num_rows > 0)
 	{
 		while($row = $result->fetch_assoc())
 		{
-			$team[] = $row;
-		}	
-
+			array_push($teams, $row['teamid']);
+		}
 	}
-	else
-	{
-		write_log("0 Results for the query: ".$sql." in .php");
-	}	
-	//for($i = 0)
 	
-	CloseCon($conn);
-}
+	$timenow = $settings['Options']['StartTime'];
+	$blocktime = 0;
 
+	$blocktime = (5 * sizeof($teams)) / $settings['Options']['TeamsPerMatch'];
+	if($blocktime > int($blocktime)) $blocktime = int($blocktime) + 1;
+
+	$times = array();
+
+	for($i = 0; $i < $settings['Options']['AnzTeams']; $i++)
+	{
+		$sql = "SELECT * FROM games WHERE team='".$teams[$i]."', finished='0', active='0' ORDER BY block ASC";
+		$result = $conn->query($sql);
+		$games = array();
+		
+		if($result->num_rows > 0)
+		{
+			while($row = $result->fetch_assoc())
+			{
+				$games[] = $row;
+			}
+		}
+		
+		if($games[0]['block'] > 1)
+		{
+			 array_push($times,addTimes($games[0]['time'],$blocktime));
+		}
+	}
+	for($k = 0; $k < sizeof($teams); $k++)
+	{
+		$sql = "UPDATE games SET time='".$times[$k]."' WHERE team='".$teams[$k]."'";
+		$conn->query($sql);
+	}
+}
 ?>
