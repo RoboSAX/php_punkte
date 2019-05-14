567f8e549f11b34c0150a39e4f1cbe41866668ec
Nightvg <night@laptop.de>
2019-05-14 22:21:03 +0200

    Added main display

---
+++ php/display/disp_main.php
@@ -0,0 +1,84 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/robosax/php/lib/db_connection.php';
+include $_SERVER['DOCUMENT_ROOT'].'/robosax/php/lib/db_manip.php';
+?>
+<head>
+<style>
+table.list {
+	border: 1px solid black; 
+	text-align: center; 
+	border-spacing: 0px;
+	margin-top: 10px;
+	}
+table.list td {
+	border: 1px solid black;
+	color: black;	
+	}
+	
+	
+table.games {
+	border: 1px solid black;
+	text-align: left; 
+	border-spacing: 0px;
+	margin-top: 10px;
+	margin-left: 20%;
+	margin-right: 20%;
+	width: 60%;
+	}
+	
+table.games td.normal {
+	border : 1px solid black;
+	color: black;
+	background-color: transparent;
+	}
+	
+table.games td.changed {
+	border: 1px solid black; 
+	color: red;
+	background-color: transparent;
+	}	
+	
+table.games td.h {
+	border: 1px solid black;
+	background-color: yellow;
+	}
+	
+table.games td.hc {
+	border: 1px solid black; 
+	color: red;
+	background-color: yellow;
+	}
+
+table.games tr {
+	
+	}
+
+table.display {
+	border: 0px solid black; 
+	text-align: center; 
+	margin-top: 10px;
+	}
+table.display td {
+	text-align: center;
+	}
+table.display tr {
+	
+}
+
+table.main {
+	text-align: left;
+}
+
+h2 {
+	text-align: center;
+	}
+</style>
+</head>
+<body>
+<table style='width:100%' class='main'>
+	<tr>
+		<td style='width:20%'><?php include $_SERVER['DOCUMENT_ROOT']."/robosax/php/display/disp_timelist.php"; ?></td>
+		<td style='width:80%'><?php include $_SERVER['DOCUMENT_ROOT']."/robosax/php/display/disp_points.php" ?></td>
+	</tr>
+</table>
+</body>
