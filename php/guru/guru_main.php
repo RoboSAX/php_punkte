634ab431090a42e867db30a28b0df079c8336451
Peter Weissig <peterweissig@github.com>
2019-05-16 18:00:21 +0200

    switched guru_gui from incude to iframe

--- php/guru/guru_main.php
+++ php/guru/guru_main.php
@@ -8,26 +8,26 @@
     <tr>
         <td style='width:20%; border-right:1px solid black' valign='top'><?php include "../display/disp_timelist.php"; ?></td>
         <td style='width:80%' valign='top'>
-            <table style='width:100%'>
+            <table style='width:100%; height:100%'>
                 <tr><td style='width:100%; border-bottom:1px solid black'> <?php include "../guru/guru_nav.php" ?> </td></tr>
-                <tr><td>
-                    <?php
-                        if($_POST['changeview'] == "teams") {
-                            include "../guru/edit_teams.php";
+                <tr><td style='width:100%; height:100%'>
+                    <iframe style='width:100%; height:100%; border: 0' src="<?php
+                            if($_POST['changeview'] == "teams") {
+                                echo "../guru/edit_teams.php";
 
-                        } elseif($_POST['changeview'] == "start") {
-                            include "../guru/edit_start.php";
+                            } elseif($_POST['changeview'] == "start") {
+                                echo "../guru/edit_start.php";
 
-                        } elseif($_POST['changeview'] == "games") {
-                            include "../guru/edit_games.php";
+                            } elseif($_POST['changeview'] == "games") {
+                                echo "../guru/edit_games.php";
 
-                        } elseif($_POST['changeview'] == "blocks") {
-                            include "../guru/edit_blocks.php";
+                            } elseif($_POST['changeview'] == "blocks") {
+                                echo "../guru/edit_blocks.php";
 
-                        } else {
-                            include "../guru/edit_games.php";
-                        }
-                    ?>
+                            } else {
+                                echo "../guru/edit_games.php";
+                            }
+                        ?>" />
                 </td></tr>
             </table>
         </td>
