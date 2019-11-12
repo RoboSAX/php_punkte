7fde2a26626d25a5ae99166dd3e307d54f902ade
Peter Weissig <peterweissig@github.com>
2019-11-13 00:44:24 +0100

    updated edit_team.php   + renamed post variable from team to team_id   + rearranged regular output and post-responds     (the last selected team stays selected after storing changes)

--- php/guru/edit_teams.php
+++ php/guru/edit_teams.php
@@ -6,18 +6,42 @@
 ?>
 
 <?php
-    $teams = new Teams;
-    if (!$teams->load_teams_from_db()) {
-        write_log("0 results for query: in edit_teams.php");
-    }
-
-    if (isset($_POST['team'])) {
-        $current_team_id = (int)$_POST['team'];
-        $current_team = $teams->get_team_by_id($current_team_id);
+    // get current team_id (if set)
+    if (isset($_POST['team_id'])) {
+        $current_team_id = (int)$_POST['team_id'];
     } else {
         $current_team_id = -1;
     }
+
+    // check if save-button was pushed
+    if (isset($_POST['changedata']) && ($current_team_id > 0)) {
+        $current_team = new Team;
+        if ($current_team->load_team_from_db($current_team_id)) {
+
+            if (isset($_POST['name']      )) $current_team->set_name      ($_POST['name']        );
+            if (isset($_POST['teamleader'])) $current_team->set_teamleader($_POST['teamleader']  );
+            if (isset($_POST['robot']     )) $current_team->set_robot     ($_POST['robot']       );
+            if (isset($_POST['active']    )) {
+                $current_team->set_active((int) $_POST['active']);
+            } else {
+                $current_team->set_active(false);
+            }
+
+            $current_team->save_team_to_db();
+        }
+    }
+
+    // load all teams and current team
+    $teams = new Teams;
+    if (!$teams->load_teams_from_db()) {
+        die("0 results for team query: in edit_teams.php");
+    }
+
+    if ($current_team_id > 0) {
+        $current_team = $teams->get_team_by_id($current_team_id);
+    }
 ?>
+
 <table>
     <tr><td>Wähle ein Team aus:</td></tr>
     <tr>
@@ -26,7 +50,7 @@
                 <div class="select" style="max-height: 150px;">
                     <?php
                         foreach ($teams->tms as $team) {
-                            echo "                    <button type='submit' name='team'";
+                            echo "                    <button type='submit' name='team_id'";
                             if ($team->get_id() == $current_team_id) {
                                 echo " style='color:blue'";
                             }
@@ -41,7 +65,7 @@
 
                 echo "        <td>\n";
                 echo "            <form action='edit_teams.php' method='post'>\n";
-                #echo "                <input type='hidden' name='team' value='".$_POST['team']."'/>";
+                echo "                <input type='hidden' name='team_id' value='".$_POST['team_id']."'/>";
                 echo "                <table>\n";
                 echo "                    <tr>\n";
                 echo "                        <td>Name: </td>\n";
@@ -62,7 +86,7 @@
                 echo "/></td>\n";
                 echo "                    </tr>\n";
                 echo "                    <tr>\n";
-                echo "                        <td colspan='2' align='center'><button name='changedata' value='".$current_team_id."' type='submit'>Speichern</button></td>\n";
+                echo "                        <td colspan='2' align='center'><button name='changedata' value='1' type='submit'>Speichern</button></td>\n";
                 echo "                    </tr>\n";
                 echo "                </table>\n";
                 echo "            </form>";
@@ -71,24 +95,5 @@
         ?>
     </tr>
 </table>
-
-<?php
-    if (isset($_POST['changedata'])) {
-        $current_team_id = (int)$_POST['changedata'];
-        $current_team = $teams->get_team_by_id($current_team_id);
-
-
-        if (isset($_POST['name']      )) $current_team->set_name      ($_POST['name']        );
-        if (isset($_POST['teamleader'])) $current_team->set_teamleader($_POST['teamleader']  );
-        if (isset($_POST['robot']     )) $current_team->set_robot     ($_POST['robot']       );
-        if (isset($_POST['active']    )) {
-            $current_team->set_active((int) $_POST['active']);
-        } else {
-            $current_team->set_active(false);
-        }
-
-        $current_team->save_team_to_db();
-    }
-?>
 
 </html>
